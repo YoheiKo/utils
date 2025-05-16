434c8d1ed458a937075928cca17496720346f299
Yohei Komori <hurumori@gmail.com>
2025-05-16 10:56:32 +0900

    first commit

---
+++ coco.bat
@@ -0,0 +1,14 @@
+@echo off
+REM ---- Open five webpages in your default browser ----
+REM (First empty pair of quotes after START is the window-title placeholder)
+
+start "" "https://coconala.com/services/3702356?ref=profile_top_service"
+start "" "https://coconala.com/services/3699274?ref=profile_top_service"
+start "" "https://coconala.com/services/3699278?ref=profile_top_service"
+start "" "https://coconala.com/services/3699233?ref=profile_top_service"
+start "" "https://coconala.com/services/3699343?ref=profile_top_service"
+start "" "https://coconala.com/services/3703510?ref=profile_top_service"
+start "" "https://coconala.com/services/3702462?ref=profile_top_service"
+
+REM Optional: keep the Command Prompt window open until you press a key
+REM pause
