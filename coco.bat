146c6bc4887488619d2627688c8bbc422cab1b78
Yohei Komori <hurumori@gmail.com>
2025-05-19 12:45:59 +0900

    Update coco.bat

--- coco.bat
+++ coco.bat
@@ -9,6 +9,6 @@ start "" "https://coconala.com/services/3699233?ref=profile_top_service"
 start "" "https://coconala.com/services/3699343?ref=profile_top_service"
 start "" "https://coconala.com/services/3703510?ref=profile_top_service"
 start "" "https://coconala.com/services/3702462?ref=profile_top_service"
-
+start "" "https://coconala.com/services/3712543"
 REM Optional: keep the Command Prompt window open until you press a key
 REM pause
